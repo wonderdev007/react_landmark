27fb0696dca83476fbc5d0ca77e1768674751014
wonderdev007 <rogerwithmail@gmail.com>
2022-04-19 22:36:29 +0300

    data organization

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Card from "./components/Card/Card";
 import Footer from "./components/Footer/Footer";
-import data from "./data";
+import data from "./data/data";
 
 export default function App() {
   const cards = data.map(item => {
