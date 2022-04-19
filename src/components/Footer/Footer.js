19253c904f72cc0a294312c28b77963e6a5d92e1
wonderdev007 <rogerwithmail@gmail.com>
2022-04-19 23:06:12 +0300

    responsive design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer>
-            <p>Made with love by Michael Kolesidis</p>
+            <p>Made with love by <a href="https://github.com/michaelkolesidis/react-landmarks">Michael Kolesidis</a></p>
         </footer>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <h1>Landmarks</h1>
-        </nav>
-    )
+  return (
+    <nav>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/3/36/Octicons-location.svg"
+        className="logo"
+        alt="logo"
+      />
+      <h1>Landmarks</h1>
+    </nav>
+  );
 }
