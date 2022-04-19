65f24b2560354968134a2f614d2dfa44b9357dcd
wonderdev007 <rogerwithmail@gmail.com>
2022-04-20 02:19:39 +0300

    github page opens in a new tab

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer>
-            <p>Made with love by <a href="https://github.com/michaelkolesidis/react-landmarks">Michael Kolesidis</a></p>
+            <p>Made with love by <a href="https://github.com/michaelkolesidis/react-landmarks" target="_blank" rel="noreferrer">Michael Kolesidis</a></p>
         </footer>
     )
 }
